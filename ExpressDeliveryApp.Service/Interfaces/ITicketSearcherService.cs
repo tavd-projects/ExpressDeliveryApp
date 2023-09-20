df32e381d5c86a4bda13c8af3cd33ff5750aa054
Timofey <97411141+tavd-projects@users.noreply.github.com>
2023-09-20 18:00:52 +0300

    Implemented service layer

---
+++ ExpressDeliveryApp.Service/Interfaces/ITicketSearcherService.cs
@@ -0,0 +1,8 @@
+﻿using ExpressDeliveryApp.Domain;
+
+namespace ExpressDeliveryApp.Service.Interfaces;
+
+public interface ITicketSearcherService
+{
+    Task<IEnumerable<Ticket>> SearchAsync(string text, int page, int pageSize);
+}
---
+++ ExpressDeliveryApp.Service/Interfaces/ITicketService.cs
@@ -0,0 +1,12 @@
+﻿using ExpressDeliveryApp.Domain;
+
+namespace ExpressDeliveryApp.Service.Interfaces;
+
+public interface ITicketService
+{
+    Guid RegisterAsync(Ticket ticket);
+    Task UpdateAsync(Ticket ticket);
+    Task CancelAsync(Guid id);
+    
+    Ticket GetAsync(Guid id);
+}
